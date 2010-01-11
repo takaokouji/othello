4f03a92ddea43a38fa5bc80cba041518848dbcdd
TAKAO Kouji <kouji@takao7.net>
2010-01-11 22:31:58 +0900

    Game#next_pieceのインタフェースを決めた。

--- app/models/game.rb
+++ app/models/game.rb
@@ -20,6 +20,26 @@ class Game < ActiveRecord::Base
   belongs_to :second_player, :class_name => "Player"
   before_save :init_board
 
+  def next_piece
+    # 次に駒を置くことができる位置の配列を求める。
+    # Playerのsolveメソッドで使うためのcontextを用意する。
+    #   contextに格納する要素
+    #   * pieces: 既存。配列の配列になっていること。
+    #   * candidate: おける場所
+    # solveをevalする。(Player#solve(context))
+    # contextに設定された駒の位置から新しいBoard(pieces)を作る。
+    pieces = [
+              [board_width / 2 - 1, board_height / 2 - 2, first_player_id],
+              [board_width / 2, board_height / 2 - 1, first_player_id],
+              [board_width / 2 - 1, board_height / 2, first_player_id],
+              [board_width / 2 - 1, board_height / 2 - 1, first_player_id],
+              [board_width / 2, board_height / 2, second_player_id],
+             ]
+    boards.create(:player => first_player, :players_context => {}, :pieces => pieces)
+  end
+
+  private
+  
   def init_board
     if boards.empty?
       boards.push(Board.new_initial_board(self))
--- test/unit/game_test.rb
+++ test/unit/game_test.rb
@@ -12,17 +12,36 @@ class GameTest < ActiveSupport::TestCase
     assert_boards_equal([Board.new_initial_board(game)], game.boards)
   end
 
+  test "next_piece:次の一手を打つ。" do
+    game = games(:game1)
+    assert_difference("game.boards.length") do
+      game.next_piece
+    end
+    pieces = [
+              [game.board_width / 2 - 1, game.board_height / 2 - 2, game.first_player_id],
+              [game.board_width / 2, game.board_height / 2 - 1, game.first_player_id],
+              [game.board_width / 2 - 1, game.board_height / 2, game.first_player_id],
+              [game.board_width / 2 - 1, game.board_height / 2 - 1, game.first_player_id],
+              [game.board_width / 2, game.board_height / 2, game.second_player_id],
+             ]
+    expected = Board.new(:player => game.first_player, :position => 2, :players_context => {}, :pieces => pieces)
+    assert_board_equal(expected, game.boards.last)
+  end
+  
   private
+
+  def assert_board_equal(expected, actual)
+    assert_equal(expected.position, actual.position)
+    assert_equal(expected.player, actual.player)
+    if expected.player
+      assert_equal(expected.players_context, actual.players_context)
+    end
+    assert_set_equal(expected.pieces, actual.pieces)
+  end
 
   def assert_boards_equal(expecteds, actuals)
     expecteds.each_with_index do |expected, index|
-      board = actuals[index]
-      assert_equal(expected.position, board.position)
-      assert_equal(expected.player, board.player)
-      if expected.player
-        assert_equal(expected.players_context, board.players_context)
-      end
-      assert_set_equal(expected.pieces, board.pieces)
+      assert_board_equal(expected, actuals[index])
     end
     assert_equal(expecteds.length, actuals.length)
   end
