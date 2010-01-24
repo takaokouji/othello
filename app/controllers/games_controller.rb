29bd7340d0dcf77a68e90e50d117997be465cb9e
TAKAO Kouji <kouji@takao7.net>
2010-01-24 11:41:03 +0900

    デフォルトで用意されているが不要なアクションを削除。

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,28 +2,6 @@
 class GamesController < ApplicationController
   before_filter :login_required
 
-  # GET /games
-  # GET /games.xml
-  def index
-    @games = Game.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @games }
-    end
-  end
-
-  # GET /games/1
-  # GET /games/1.xml
-  def show
-    @game = Game.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @game }
-    end
-  end
-
   # GET /games/new
   # GET /games/new.xml
   def new
@@ -33,11 +11,6 @@ class GamesController < ApplicationController
       format.html # new.html.erb
       format.xml  { render :xml => @game }
     end
-  end
-
-  # GET /games/1/edit
-  def edit
-    @game = Game.find(params[:id])
   end
 
   # POST /games
@@ -54,35 +27,6 @@ class GamesController < ApplicationController
         format.html { render :action => "new" }
         format.xml  { render :xml => @game.errors, :status => :unprocessable_entity }
       end
-    end
-  end
-
-  # PUT /games/1
-  # PUT /games/1.xml
-  def update
-    @game = Game.find(params[:id])
-
-    respond_to do |format|
-      if @game.update_attributes(params[:game])
-        flash[:notice] = 'Game was successfully updated.'
-        format.html { redirect_to(@game) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @game.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /games/1
-  # DELETE /games/1.xml
-  def destroy
-    @game = Game.find(params[:id])
-    @game.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(games_url) }
-      format.xml  { head :ok }
     end
   end
 
